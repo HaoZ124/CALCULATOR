75a0e210b69105698d36092f838d749fd905bb8d
HaoZ124 <zhouhao927@gmail.com>
2022-07-05 13:38:53 -0600

    Calculator is functional except keypress function and style

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CALCULATOR
 Project-calculator for odin project
+Algorithm:
+1. Get user's input
+2. if num1 = 0, convert string to num1
+3. if press the operate key, put value to op then switch to num2
+4. any input goes to num2
+5. if press the operate key again or press equal key, calculate num1 and num2 then put to num1
+6. keep convert the string to num2, once pressing the operate or equal then calculate 2 numbers
--- app.js
+++ app.js
@@ -1,54 +1,95 @@
-//Get the value from pressing key
-function getValue(a) {
-    keyInput.result += a;
-    console.log(keyInput.result);
+const input = {
+    currentNum: '',
+    previousNum: '',
+    op: '',
 }
 
-var keyInput = {
-    num1: 10,
-    num2: 10,
-    op: '*',
-    result: 0,
+const upper = document.querySelector('.upper');
+const bottom = document.querySelector('.bottom');
+
+//Get the value from pressing key
+function getValue(a) {
+    if (a !== '.' || !input.currentNum.includes('.')) {
+        if (input.currentNum.length <= 10) {
+            input.currentNum += a;
+        }
+    }
+    upper.textContent = input.currentNum;
+}
+
+function getOperate(b) {
+    if (input.currentNum !== '') {
+        if (input.previousNum !== '') {
+            compute(input);
+        }
+    }
+    input.op = b;
+    input.previousNum = input.currentNum;
+    input.currentNum = '';
+    upper.append(input.op);
 }
 
 
 //Make function calculate numbers
-function operate(input) {
+function compute(input) {
+    var pre = parseFloat(input.previousNum);
+    var cur = parseFloat(input.currentNum);
+
+    if (isNaN(pre) || isNaN(cur)) return
     switch (input.op) {
         case '+':
-            input.result = input.num1 + input.num2;
-            break;
+            pre += cur;
+            break
         case '-':
-            input.result = input.num1 - input.num2;
-            break;
+            pre -= cur;
+            break
         case '*':
-            input.result = input.num1 * input.num2;
-            break;
+            pre *= cur;
+            break
         case '/':
-            if (input.num2 !== 0) {
-                input.result = input.num1 / input.num2;
+            if (cur !== 0) {
+                pre /= cur;
             } else {
-                input.result = 'Error, cannot be divided by 0!'
+                input.currentNum = 'Error, cannot be divided by 0!'
             }
     }
-    return input.result;
+    pre = roundNum(pre);
+    input.currentNum = pre.toString();
+    bottom.textContent = input.currentNum;
+    input.previousNum = '';
+    input.op = '';
+
 }
 
-//Display the result
-function displayResult(input) {
-    var display = document.querySelector('.display');
-    display.textContent = operate(input);
+function roundNum(num) {
+    return Math.round(num * 100000) / 100000;
 }
-displayResult(keyInput);
+
+var enter = document.querySelector('#enter');
+enter.addEventListener('click', () => {
+    if (input.currentNum !== '' && input.previousNum !== '') {
+        compute(input);
+    }
+});
+
 //Function to clear all the input and output
 var clear = document.querySelector('.clear');
 clear.addEventListener('click', clearAll);
 
 function clearAll() {
     var display = document.querySelector('.display');
-    keyInput.num1 = 0;
-    keyInput.num2 = 0;
-    keyInput.op = '';
-    keyInput.result = 0;
-    display.textContent = 0;
+    input.currentNum = '';
+    input.previousNum = '';
+    input.op = '';
+    upper.textContent = '';
+    bottom.textContent = 0;
+}
+
+const del = document.querySelector('.del');
+del.addEventListener('click', deleteNum);
+
+function deleteNum() {
+    input.currentNum = input.currentNum.slice(0, -1);
+    console.log(input);
+    upper.textContent = input.currentNum;
 }
